5b9bc64d0276a49bd61b06fbfabef9adc4b38d8f
ckvignesh <vigneshckprakash@gmail.com>
2022-04-18 23:28:21 +0530

    test

--- src/components/DarkFront.js
+++ src/components/DarkFront.js
@@ -4,6 +4,14 @@ export default function DarkFront() {
   return (
     <div className="dark-front-layer-one">
       <h1>Front Dark</h1>
+      <div className="dark-front-middle-box">
+        <div className="dark-front-desc-box">
+          About I am a frontend developer with a particular interest in making
+          things simple and automating daily tasks. I try to keep up with
+          security and best practices, and am always looking for new things to
+          learn.
+        </div>
+      </div>
     </div>
   );
 }
